b01b5e0048f1eebeb66ae78df19a3a857c1a4a7f
Uriel Aispuro <urielalexis64@gmail.com>
2021-04-01 00:05:59 -0700

    show history from local json file

--- index.js
+++ index.js
@@ -17,31 +17,38 @@ const main = async () => {
 				const term = await readInput("Enter a city: ");
 
 				// * Search places
+				console.log("loading...");
 				const places = await searches.searchCity(term);
+				console.clear();
 
 				// * Select place
 				const placeId = await listPlaces(places);
-				if (placeId === 0) break;
+				if (placeId === 0) continue;
 
 				const {name, lat, lng} = places.find((place) => place.id === placeId);
 
+				searches.addHistory(name);
 				// * Weather
+				console.log("loading...");
 				const {desc, temp, min, max} = await searches.placeWeather(lat, lng);
 
 				// * All results
 				console.clear();
-				console.log("\nPlace info\n".bgGreen.white);
+				console.log("\n  Place info  \n".bgYellow.black);
 				console.log("Place: ", name.yellow);
 				console.log("Latitude: ", lat);
 				console.log("Longitude: ", lng);
-				console.log("Temperature: ", temp);
-				console.log("Min: ", min);
-				console.log("Max: ", max);
+				console.log("Temperature: ", temp, "°C");
+				console.log("Min: ", min, "°C");
+				console.log("Max: ", max, "°C");
 				console.log("Description: ", desc.yellow);
 
 				break;
 			case 2:
-				///TODO: show history
+				searches.history.forEach((place, index) => {
+					const idx = `${index + 1}.`.green;
+					console.log(`${idx} ${place}`);
+				});
 				break;
 			default:
 		}
