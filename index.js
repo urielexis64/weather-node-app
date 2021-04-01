5ec5bd283f58040fb3d76296a334781992d2ecc6
Uriel Aispuro <urielalexis64@gmail.com>
2021-03-31 21:22:43 -0700

    Getting and showing place info

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-const {inquirerMenu, pause, readInput} = require("./helpers/inquirer");
+const {inquirerMenu, pause, readInput, listPlaces} = require("./helpers/inquirer");
 const Searches = require("./models/searches");
 
 const main = async () => {
@@ -13,13 +13,29 @@ const main = async () => {
 		opt = await inquirerMenu();
 		switch (opt) {
 			case 1:
-				const city = await readInput("Enter a city: ");
-				await searches.searchCity(city);
+				// * Show prompt message
+				const term = await readInput("Enter a city: ");
+
+				// * Search places
+				const places = await searches.searchCity(term);
+
+				// * Select place
+				const placeId = await listPlaces(places);
+				const selectedPlace = places.find((place) => place.id === placeId);
+				// * Weather
+
+				// * All results
+				console.log("Place info\n".green);
+				console.log("Place: ", selectedPlace.name);
+				console.log("Latitude: ", selectedPlace.lat);
+				console.log("Longitude: ", selectedPlace.lng);
+				console.log("Temperature: ");
+				console.log("Min: ");
+				console.log("Max: ");
+
 				break;
 			case 2:
 				///TODO: show history
-				break;
-			case 0:
 				break;
 			default:
 		}
